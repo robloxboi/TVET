d38162737152b640961f69231ad7a3a79f631020
gabriel.perrino1 <gabstarjuventus@gmail.com>
2018-05-22 15:56:07 +1000

    Started creating Minesweeper

---
+++ UNITY/Assets/~Minesweeper/Scripts/Grid.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper
+{
+    public class Grid : MonoBehaviour
+    {
+        public GameObject tilePrefab;
+        public int width = 10, height = 10;
+        public float spacing = .155f;
+
+        private Tile[,] tiles;
+        Tile SpawnTile(Vector3 pos)
+        {
+            GameObject clone = Instantiate(tilePrefab);
+
+            clone.transform.position = pos;
+            Tile currentTile = clone.GetComponent<Tile>();
+
+            return currentTile;
+        }
+        void GenerateTiles()
+        {
+            tiles = new Tile[width, height];
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Vector2 halfsize = new Vector2(width * 0.5f,
+                                                   height * 0.5f);
+                    Vector2 pos = new Vector2(x - halfsize.x,
+                                              y - halfsize.y);
+                    pos *= spacing;
+                    Tile tile = SpawnTile(pos);
+                    tile.transform.SetParent(transform);
+                    tile.x = x;
+                    tile.y = y;
+                    tiles[x, y] = tile;
+                }
+            }
+        }
+        void Start()
+        {
+            GenerateTiles();
+        }
+    }
+}
