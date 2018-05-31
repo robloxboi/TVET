bf7b47db420c24c9627a2a645e7f1ace4ddfe225
Gabriel.Perrino <gabstarjuventus@gmail.com>
2018-05-31 10:27:16 +1000

    test

--- UNITY/Assets/~Minesweeper/Scripts/Tile.cs
+++ UNITY/Assets/~Minesweeper/Scripts/Tile.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Minesweeper
+namespace Minesweepe
 {
     [RequireComponent(typeof(SpriteRenderer))]
     public class Tile : MonoBehaviour
