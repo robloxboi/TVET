12ec220cf2e62515a5a54af67dc89e3b8fd1b35a
gabriel.perrino1 <gabstarjuventus@gmail.com>
2018-06-05 15:44:20 +1000

    Started Assessment

---
+++ UNITY/Assets/~Assessments/Assessment1/Scripts/Bird.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    public float upForce = 200f;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (isDead == false )
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+            }
+        }
+	}
+    void OnCollisionEnter2D ()
+    {
+        isDead = true;  
+    }
+}
